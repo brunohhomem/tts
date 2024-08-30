95db442a6c20045a69edd648183df319bdccb17d
Bruno H. Homem <brunohhomem@gmail.com>
2024-08-30 07:27:47 -0300

    feat: added upload endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateMeasure } from './types';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/upload')
+  async createMeasure(@Body() data: CreateMeasure) {
+    return this.appService.createMeasure(data);
   }
+
+  // @Post()
+  // async createUser(@Body() data: Prisma.UserCreateInput) {
+  //   return this.userService.createUser(data);
+  // }
+
+  // @Patch(':id')
+  // async updateUser(
+  //   @Param('id') id: string,
+  //   @Body() data: Prisma.UserUpdateInput,
+  // ) {
+  //   return this.userService.updateUser(Number(id), data);
+  // }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,81 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateMeasure, MeasureResponse } from './types';
+import {
+  getImageURL,
+  hasData,
+  isBase64,
+  isUUID,
+  validateMonth,
+  validateType,
+} from './utils/validation';
+import { runGemini } from './utils/gemini';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async createMeasure(data: CreateMeasure): Promise<MeasureResponse> {
+    hasData(data);
+    isBase64(data.image);
+    isUUID(data.customer_code);
+
+    const insertDate = new Date(data.measure_datetime);
+
+    validateMonth(data.customer_code, data.measure_type, insertDate);
+    validateType(data.measure_type);
+
+    const geminiResult = runGemini(data.measure_type, data.image);
+    // isMeter(await geminiResult);
+    // const numberResult = Number(geminiResult);
+
+    // Verifica se o customer_code já existe
+    let customer = await this.prisma.customer.findUnique({
+      where: { customer_code: data.customer_code },
+    });
+
+    // Se o cliente não existir, cria um novo cliente
+    if (!customer) {
+      customer = await this.prisma.customer.create({
+        data: { customer_code: data.customer_code },
+      });
+    }
+
+    // Adiciona a nova medida à tabela `measure`
+    const measure = await this.prisma.measure.create({
+      data: {
+        measure_value: Number(geminiResult),
+        measure_datetime: data.measure_datetime,
+        measure_type: data.measure_type,
+        has_confirmed: false,
+        image_url: getImageURL(data.image),
+        customer: {
+          connect: { customer_code: data.customer_code }, // Associa a medida ao cliente
+        },
+      },
+      select: {
+        image_url: true,
+        measure_value: true,
+        measure_uuid: true, // Certifique-se de que este campo existe na tabela
+      },
+    });
+
+    return measure;
   }
 }
+
+// async function findCustomer(customer_id: string) {
+//   constructor(private prisma: PrismaService) {}
+
+//   // Verifica se o customer_code já existe
+//   let customer = await this.prisma.customer.findUnique({
+//     where: { customer_code: customer_id },
+//   });
+
+//   // Se o cliente não existir, cria um novo cliente
+//   if (!customer) {
+//     customer = await this.prisma.customer.create({
+//       data: { customer_code: customer_id },
+//     });
+//   }
+// }
