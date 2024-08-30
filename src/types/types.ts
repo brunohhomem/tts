23ebbacefbc8427a1ce5534fcbf4354965bcf434
Bruno H. Homem <brunohhomem@gmail.com>
2024-08-29 21:46:19 -0300

    feat: added `types`

---
+++ src/types/types.ts
@@ -0,0 +1,30 @@
+import { MeasureType } from '@prisma/client';
+
+export interface MeasureProps {
+  measure_uuid: string;
+  measure_value: number;
+  measure_unit: string;
+  measure_datetime: Date;
+  measure_type: MeasureType; // Enum: "WATER" ou "GAS"
+  has_confirmed: boolean;
+  image_url: string;
+  customer_code: string;
+}
+
+export interface CreateMeasure {
+  // image: string | File;
+  image: string;
+  customer_code: string;
+  measure_datetime: Date;
+  measure_type: MeasureType; // Enum: "WATER" ou "GAS"
+}
+
+export interface ConfirmMeasure {
+  measure_uuid: string;
+  confirmed_value: number;
+}
+
+export interface CustomerProps {
+  customer_code: string;
+  measures: [];
+}
